2fa91ae0cc7449149f11c0620804ab6435c38b80
Kokoulin Aleksei <kokoulin21@gmail.com>
2020-07-12 22:33:52 +0300

    Update MyFirstTest.java

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -12,30 +12,30 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class MyFirstTest {
+public class MySecondTest {
 
     private WebDriver driver;
     private WebDriverWait wait;
 
 
-    @Before
-    public void start(){
+@Before
+public void start(){
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
-    }
+wait = new WebDriverWait(driver, 10);
+}
 
     @Test
-    public void myFirstTest() {
+public void myFirstTest() {
         driver.get("http://localhost/litecart/");
-        wait.until(titleIs("My Store | Online Store"));
+wait.until(titleIs("My Store | Online Store"));
         int products = driver.findElements(By.xpath("//div[@class=\"image-wrapper\"]")).size();
         int stickers = driver.findElements(By.xpath("//div[@class=\"image-wrapper\"]/div[contains(@class, 'sticker')]")).size();
-        Assert.assertTrue(products == stickers);
-    }
+Assert.assertTrue(products == stickers);
+}
 
     @After
-    public void stop(){
+public void stop(){
         driver.quit();
-        driver= null;
-    }
+driver= null;
+}
 }
